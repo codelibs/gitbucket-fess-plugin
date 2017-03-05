304b3662c6d1b6a15f7bca784e14affb5b454a0b
Keiichi Watanabe <udon.watanabe@gmail.com>
2017-03-05 21:51:19 +0900

    Change label in global menu
    "Search" -> "Global Search"

--- src/main/scala/Plugin.scala
+++ src/main/scala/Plugin.scala
@@ -20,7 +20,7 @@ class Plugin extends gitbucket.core.plugin.Plugin {
   )
 
   override val globalMenus = Seq(
-    (context: Context) => Some(Link("search", "Search", "fess?q="))
+    (context: Context) => Some(Link("fess", "Global Search", "fess?q="))
   )
 
   override val assetsMappings = Seq("/fess/assets" -> "/fess/assets")
